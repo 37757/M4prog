c742dd95c2472bf951ae34afb232ef5526ecf6ae
37757 <37757@ma-web.nl>
2025-05-25 21:04:49 +0200

    4klaar

---
+++ 01 opstarten/dockerM4Prog/sql/03 where/01 where.sql
@@ -0,0 +1,5 @@
+select * from huisdieren where leeftijd <= 4;
+select * from huisdieren where leeftijd > 4;
+select * from huisdieren where leeftijd = 4;
+
+select * from huisdieren where naam = 'julian';
